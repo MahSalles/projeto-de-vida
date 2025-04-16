cbe6483e2d916848818a41e64773c6a19cf0cd8f
MahSalles <145451172+MahSalles@users.noreply.github.com>
2025-04-16 08:05:24 -0300

    a

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-12-05T00:00:00");
-let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
